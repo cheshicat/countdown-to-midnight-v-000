82e936796bd100fd6f3c43dcf11638aec2c3c164
Allison Howard <alliemhow@gmail.com>
2018-08-29 13:20:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,14 @@
-def countdown
+def countdown(number)
   number = 0
   while number < 11
     puts "#{number} SECOND(S)!"
     number +=1
   end
   return "HAPPY NEW YEAR!"
+end
+
+x = 1
+while x < 10
+  puts "#{x} is less than 10"
+  x += 1
 end
